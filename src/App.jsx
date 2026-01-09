880e9eb274ecd7de356826e51439fb341777e0be
Ben Samy <bensamy10@gmail.com>
2026-01-08 22:08:33 -0800

    Set up app layout with Home, Navbar, and Footer components. Imported assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div>App</div>
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App = () => {
+  return(
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+  )
+
 }
 
-export default App
+export default App;
---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,15 @@
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { Box } from '@mui/material';
+
+// import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+// import Detail from '../components/Detail';
+// import ExerciseVideos from '../components/ExerciseVideos';
+// import SimilarExercises from '../components/SimilarExercises';
+
+const ExerciseDetail = () => {
+  return(
+    <div>Exercise Detail</div>
+  )
+}
+export default ExerciseDetail;
