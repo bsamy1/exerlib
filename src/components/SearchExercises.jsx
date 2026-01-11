5cc9b83c83ee980536667dc80390b812466c01e2
Ben Samy <bensamy10@gmail.com>
2026-01-11 15:54:44 -0800

    Add logic to SearchExercises component, create fetchData util file, and add RAPID API key

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,64 @@
+import React, { useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { fetchData, exerciseOptions } from '../utils/fetchData';
+
 const SearchExercises = () => {
-    return (
-        <div>SearchExercises
-        </div>
-    )
+  const [search, setSearch] = useState('');
+
+  const handleSearch = () => {
+    console.log('Searching for:', search);
+  };
+
+  return (
+    <Stack
+      alignItems="center"
+      mt="37px"
+      justifyContent="center"
+      p="20px"
+    >
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: '700',
+            },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Exercises"
+          type="text"
+        />
+
+        <Button
+          onClick={handleSearch}
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '173px', xs: '80px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0px',
+            fontSize: { lg: '20px', xs: '14px' },
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
---
+++ src/utils/fetchData.jsx
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+		'x-rapidapi-key': import.meta.env.RAPID_API_KEY,
+	}
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    
+    return data;
+}
