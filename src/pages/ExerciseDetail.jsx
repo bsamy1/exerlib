368f98b0b4969bab3c5599150793755d119af23c
Ben Samy <bensamy10@gmail.com>
2026-01-25 21:13:08 -0800

    Add Pagination to exercise results and include useParams to ExerciseDetail component

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,18 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData} from '../utils/fetchData';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const { id } = useParams(); 
+  const [exerciseDetail, setExerciseDetail] = useState({}); 
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseData = await fetchData(
+        `https://exercisedb.p.rapidapi.com/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseData);
+    };
+
+    fetchExerciseData();
+  }, [id]); 
+
   return (
     <Box>
-      <Detail />
-      <ExerciseVideos/>
-      <SimilarExercises />
+      <Detail exercise={exerciseDetail} />
+      <ExerciseVideos exerciseName={exerciseDetail.name} />
+      <SimilarExercises 
+        target={exerciseDetail.target} 
+        bodyPart={exerciseDetail.bodyPart} 
+      />
     </Box>
-  )};
+  );
+};
 
 export default ExerciseDetail;
